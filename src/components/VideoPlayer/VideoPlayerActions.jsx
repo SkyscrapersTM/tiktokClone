6138796933034f2cca2a78dca1f60f3249c0d516
SkyscrapersTM <74199653+SkyscrapersTM@users.noreply.github.com>
2023-02-24 20:25:14 -0500

    feat(Root): Use new Icons

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,17 +1,27 @@
 import styles from './styles.module.css'
 import Heart from '../Icons/Heart'
+import { Comments } from '../Icons/Comments'
+import { Share } from '../Icons/Share'
 
-export default function VideoPlayerActions () {
+export default function VideoPlayerActions ({
+  likes = 2041,
+  comments = 333,
+  shares = 30,
+  hearted = false
+}) {
   return (
     <aside className={styles.actions}>
       <div className={styles.action}>
         <Heart />
+        <strong title='like'>{likes}</strong>
       </div>
       <div className={styles.action}>
-        <Heart />
+        <Comments />
+        <strong title='comments'>{comments}</strong>
       </div>
       <div className={styles.action}>
-        <Heart />
+        <Share />
+        <strong title='share'>{shares}</strong>
       </div>
     </aside>
   )
