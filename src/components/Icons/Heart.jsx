65008a58959a57d0dbffe0f80b7e23bd401fccfd
SkyscrapersTM <74199653+SkyscrapersTM@users.noreply.github.com>
2023-02-24 19:25:07 -0500

    feat(Root): Add new Heart icon

---
+++ src/components/Icons/Heart.jsx
@@ -0,0 +1,14 @@
+const Heart = () => {
+  return (
+    <svg width='2em' data-e2e='' height='2em' viewBox='0 0 48 48'>
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M15 4.5C21 4.5 24 8.5 24 8.5C24 8.5 27 4.5 33 4.5C40 4.5 45 9.99998 45 17C45 25 38.4622 32.1314 32.5 37.5C28.8191 40.8144 26 43 24 43C22 43 19.101 40.7978 15.5 37.5C9.63898 32.1325 3 25 3 17C3 9.99998 8 4.5 15 4.5Z'
+        fill='currentColor'
+      />
+    </svg>
+  )
+}
+
+export default Heart
